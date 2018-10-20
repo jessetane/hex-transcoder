bf43c909df583dc3922cdd51758738cf7fc35923
Jesse Tane <jesse.tane@gmail.com>
2018-10-20 10:26:04 -0400

    improve encoding performance ~2.2x by using a larger lookup table thanks to @jimmywarting for the idea

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-var encodeLookup = '0123456789abcdef'.split('')
+var alphabet = '0123456789abcdef'.split('')
+var encodeLookup = []
 var decodeLookup = []
-var i = 0
-while (i < 10) decodeLookup[0x30 + i] = i++
-while (i < 16) decodeLookup[0x61 - 10 + i] = i++
+
+for (var i = 0; i < 256; i++) {
+  encodeLookup[i] = alphabet[i >> 4 & 0xf] + alphabet[i & 0xf]
+  if (i < 16) {
+    if (i < 10) {
+      decodeLookup[0x30 + i] = i
+    } else {
+      decodeLookup[0x61 - 10 + i] = i
+    }
+  }
+}
 
 exports.encode = function (array) {
   var length = array.length
   var string = ''
-  var c, i = 0
+  var i = 0
   while (i < length) {
-    c = array[i++]
-    string += encodeLookup[(c & 0xF0) >> 4] + encodeLookup[c & 0xF]
+    string += encodeLookup[array[i++]]
   }
   return string
 }
