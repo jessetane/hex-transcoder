5e5ce07afb898d6472d5fdc201b6fc89b0a9c20f
Jimmy Wärting <jimmy@warting.se>
2019-02-02 01:11:23 +0100

    added JSDoc
    helps with IDE

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ for (var i = 0; i < 256; i++) {
   }
 }
 
+/**
+ * Encode a Uint8Array to a hex string
+ * 
+ * @param  {Uint8Array} array Bytes to encode to string
+ * @return {String}           hex string
+ */
 exports.encode = function (array) {
   var length = array.length
   var string = ''
@@ -23,6 +29,12 @@ exports.encode = function (array) {
   return string
 }
 
+/**
+ * Decodes a hex string to a Uint8Array
+ * 
+ * @param  {String} string hex string to decode to Uint8Array
+ * @return {Uint8Array}    Uint8Array
+ */
 exports.decode = function (string) {
   var sizeof = string.length >> 1
   var length = sizeof << 1
